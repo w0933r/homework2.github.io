6a115524927043185d909e94784501abe6b5fe01
w0933r <132487196+w0933r@users.noreply.github.com>
2023-05-04 01:07:01 +0800

    Add files via upload

---
+++ 網站編輯作業/main.js
@@ -0,0 +1,6 @@
+// 使用原生JavaScript選到class="navbar-btn"的按鈕並為其綁定click事件 
+document.querySelector(".btn").addEventListener("click", function () {
+    // 表達點擊後要做的動作...     
+    // 選到 class="navbar-list" 並為其切換 active 的 class     
+    ;
+});
